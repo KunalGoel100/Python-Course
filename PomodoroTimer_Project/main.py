0587e2b1878a9115ffd4f768618bd068d0da7b48
Kunal <kunal2000goel@gmail.com>
2026-01-20 18:22:00 +0530

    Pomodoro timer project

---
+++ PomodoroTimer_Project/main.py
@@ -0,0 +1,119 @@
+from tkinter import *
+
+from numpy.ma.core import shape
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+BLACK = "#000000"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+count=0
+BCount = 0
+countdown = None
+TimerTime = 1
+BreakTime = 0.5
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def call_Timer():
+    Timer(TimerTime*60)
+def call_Break():
+    Timer(int(BreakTime*60))
+def reset_timer():
+    screen.after_cancel(countdown)
+    canvas.itemconfig(Timer_text, text=f"00:00")
+    global count
+    global BCount
+    count = 0
+    BCount = 0
+    disp_blank()
+    disp_Timer()
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def Timer(tim):
+    disp_Work()
+    count_min = int(tim / 60)
+    count_sec = tim % 60
+    format = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09"]
+    if count_sec < 10 and count_sec >= 0:
+        count_sec = format[count_sec]
+    if count_min < 10 and count_min >= 0:
+        count_min = format[count_min]
+
+    canvas.itemconfig(Timer_text, text=f"{count_min}:{count_sec}")
+    if tim>0:
+        global countdown
+        countdown = screen.after(1000, Timer, tim-1)
+    elif tim<=0:
+        global count
+        global BCount
+        if count < 4 and BCount == 0:
+            Tick(count)
+            call_Break()
+            BCount = 1
+            disp_Break()
+        elif count < 4:
+            disp_Timer()
+            BCount = 0
+
+            print(count)
+            count += 1
+            call_Timer()
+        else:
+            Tick(count)
+            canvas.itemconfig(Timer_text, text=f"OVER")
+            print("Over")
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+screen = Tk()
+screen.title("Pomodoro Timer")
+screen.config(bg="#f7f5dd")
+screen.minsize(width=400,height=350)
+
+
+# screen.after(1000,Timer,10)
+def Tick(count):
+    for i in range(0,count+1,1):
+        tick = Label(text="✔️", bg=YELLOW)
+        tick.place(x=150+i*20,y=300)
+
+def disp_blank():
+    blank = Button(text="",padx=50,bg=YELLOW,border=0)
+    blank.place(x=150,y=300)
+def disp_Break():
+    text.config(text="Break",fg=PINK)
+def disp_Timer():
+    text.config(text="Timer",fg=BLACK)
+def disp_Work():
+    text.config(text="Work",fg=RED)
+
+text = Label(text="Timer",font=("Courier",30,"bold"),bg="#f7f5dd",fg=BLACK)
+# text.config(fg=GREEN)
+text.grid(row=1, column=2)
+startButton = Button(text="Start",padx=10, bg=GREEN, border=1)
+startButton.place(x=80,y=300)
+startButton.config(command=call_Timer)
+resetButton = Button(text="Reset",padx=10, bg=GREEN, border=1)
+resetButton.place(x=270,y=300)
+resetButton.config(command=reset_timer)
+
+
+
+canvas = Canvas(width=400, height=245)
+canvas.config(bg="#f7f5dd", highlightthickness=0)
+tomato = PhotoImage(file="tomato.png")
+canvas.create_image(200,120,image=tomato)
+Timer_text=canvas.create_text(200,140,text="00:00",font=("Courier",30,"bold"))
+canvas.grid(row=2, column=2)
+
+
+
+# Timer(0)
+screen.mainloop()
---
+++ PomodoroTimer_Project/try.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+screen = Tk()
+
+
+text = Label(text="hi")
+text.pack()
+text.
+screen.mainloop()
